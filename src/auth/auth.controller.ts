1009cf51a5603f5b4c8c79500fdc88cf2f1ef63a
LauchoIT <contacto@gmail.com>
2024-03-24 09:37:38 -0400

    Add Vlidation in register and login

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Inject, Logger, Post } from '@nestjs/common';
+import { Body, Controller, Get, Inject, Logger, Post } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
 import { NATS_SERVICES } from 'src/config';
+import { LoginDto, RegisterDto } from './dto';
 
 @Controller('auth')
 export class AuthController {
@@ -9,19 +10,19 @@ export class AuthController {
   constructor(@Inject(NATS_SERVICES) private readonly client: ClientProxy) {}
 
   @Post('register-user')
-  async registerUser(@Body() registerUserDto: any) {
+  async registerUser(@Body() registerUserDto: RegisterDto) {
     const register = this.client.send('auth.register.user', registerUserDto);
     return register;
   }
 
   @Post('login-user')
-  loginUser(@Body() loginDto: any) {
+  loginUser(@Body() loginDto: LoginDto) {
     console.log(loginDto);
     const login = this.client.send('auth.login.user', loginDto);
     return login;
   }
 
-  @Post('verify-user')
+  @Get('verify-user')
   verifyToken() {
     const verify = this.client.send('auth.verify.user', {});
     return verify;
